cc45820fb468326150cbf97188f22e4a11886012
malaksabry15 <148697224+malaksabry15@users.noreply.github.com>
2026-04-13 13:00:02 +0200

    Update Home.jsx

--- react-App2/src/components/home/Home.jsx
+++ react-App2/src/components/home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Card from '../card/card'
+import Card from '../card/Card'
 import Header from '../header/Header'
 
 const Home = ({meals ,getdata,getCategoryMeals,getdetails}) => {
