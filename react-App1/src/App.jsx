8b5503111415cbbf72e08c0ec779085917a030a5
malaksacry15 <malaksabry4002@gmail.com>
2026-04-13 12:15:05 +0200

    Fix error

--- react-App1/src/App.jsx
+++ react-App1/src/App.jsx
@@ -1,10 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// import './App.css'
-import Navbar from './components/navbar/Navbar'
-import Header from './components/Header'
-import Footer from './components/footer/Footer'
+
+
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import About from './components/about/About'
 import Portfoilo from './components/portfoilo/Portfoilo'
