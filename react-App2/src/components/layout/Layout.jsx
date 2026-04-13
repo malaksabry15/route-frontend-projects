5de14d1d8ca68536c21a28de92bc561df9170941
malaksabry15 <148697224+malaksabry15@users.noreply.github.com>
2026-04-13 13:08:04 +0200

    Update Layout.jsx

--- react-App2/src/components/layout/Layout.jsx
+++ react-App2/src/components/layout/Layout.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Header from '../header/Header'
+
 import { Outlet } from 'react-router-dom'
 import Footer from '../footer/Footer'
 import Sidebar from '../sidabar/Sidebar'
-import Card from '../card/card'
+
 
 const Layout = ({getdata , getCategoryMeals, meals}) => {
   return <>
